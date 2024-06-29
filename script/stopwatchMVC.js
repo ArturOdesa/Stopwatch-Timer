d6274bab031f9e3e86f333bd20fc5b7d4fed816f
Artur Stoianov <stoianovart@gmail.com>
2024-06-29 22:28:20 +0300

    add MVC for stopwatch

---
+++ script/stopwatchMVC.js
@@ -0,0 +1,103 @@
+class stopwatchModel {
+    constructor() {
+        this.time = 0;
+        this.stopwatch = null;
+    }
+
+    start() {
+        if (!this.stopwatch) {
+            this.stopwatch = setInterval(() => {
+                this.time++;
+                this.notifyObservers();
+            }, 1000);
+        }
+    }
+
+    stop() {
+        if (this.stopwatch) {
+            clearInterval(this.stopwatch);
+            this.stopwatch = null;
+        }
+    }
+
+    reset() {
+        this.stop();
+        this.time = 0;
+        this.notifyObservers();
+    }
+
+    getTime() {
+        return this.time;
+    }
+
+    notifyObservers() {}
+
+    isRunning() {
+        return this.stopwatch !== null;
+    }
+}
+
+class stopwatchView {
+    constructor() {
+        this.timeElement = document.querySelector('#stopwatch-time');
+        this.startStopBtn = document.querySelector('#stopwatch-start-stop-btn');
+        this.resetBtn = document.querySelector('#stopwatch-reset-btn');
+        this.stopwatchArrow = document.querySelector('#stopwatch-arrow');
+    }
+
+    formatTime(time) {
+        const hours = String(Math.floor(time / 3600)).padStart(2, '0');
+        const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0');
+        const seconds = String(time % 60).padStart(2, '0');
+        const degree = seconds * 6;
+        this.stopwatchArrow.style.transform = `translateX(-50%) rotate(${degree}deg)`;
+        return `${hours}:${minutes}:${seconds}`;
+    }
+
+    update (time, isRunning) {
+        this.timeElement.textContent = this.formatTime(time);
+        this.startStopBtn.textContent = isRunning ? 'Stop' : 'Start';
+    }
+
+    bindStartStop(handler) {
+        this.startStopBtn.addEventListener('click', handler);
+    }
+
+    bindReset(handler) {
+        this.resetBtn.addEventListener('click', handler);
+    }
+}
+
+class stopwatchController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        this.view.bindStartStop(this.handleStartStop.bind(this));
+        this.view.bindReset(this.handleReset.bind(this));
+        this.model.notifyObservers = this.updateView.bind(this);
+        this.updateView();
+    }
+
+    handleStartStop() {
+        if (this.model.isRunning()) {
+            this.model.stop();
+        } else {
+            this.model.start();
+        }
+    }
+
+    handleReset() {
+        this.model.reset();
+    }
+
+    updateView () {
+        this.view.update(this.model.getTime(), this.model.isRunning());
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const model = new stopwatchModel();
+    const view = new stopwatchView();
+    const controller = new stopwatchController(model, view);
+})
